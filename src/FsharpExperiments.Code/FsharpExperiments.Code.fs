cc82ef7ea1372b3274269edc86dc6ed2a18a6bd6
dominikus1993 <dominikus1910@gmail.com>
2016-06-18 11:13:57 +0200

    added green test for euler1 solution

--- src/FsharpExperiments.Code/FsharpExperiments.Code.fs
+++ src/FsharpExperiments.Code/FsharpExperiments.Code.fs
@@ -1,4 +1,5 @@
 namespace FsharpExperiments.Code
 
 module Euler1 = 
-    let solve (start, quantity) = 2
+    let solve (start, quantity) = 
+        [start..quantity] |> List.filter(fun x -> x % 5 = 0 || x % 3 = 0) |> List.sum
