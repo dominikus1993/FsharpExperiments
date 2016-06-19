02be6d0b76bcb154515a837ee9b89e08c5f92b59
dominikus1993 <dominikus1910@gmail.com>
2016-06-19 11:16:55 +0200

    added solution for third problem of ninetynine problems

--- src/FsharpExperiments.Code/NinetyNine.fs
+++ src/FsharpExperiments.Code/NinetyNine.fs
@@ -1,6 +1,10 @@
 namespace FsharpExperiments.Code.NinetyNine
-    module First = 
+    module Problem1 = 
         let solve(list) = list |> List.rev |> List.head
     
     module Problem2 = 
         let solve(list) = list |> List.rev |> List.tail |> List.head
+
+    module Problem3 =
+        let solve(list, elementIndex) = 
+            list |> List.nth elementIndex
--- test/FsharpExperiments.Tests/NinetyNine.Tests.fs
+++ test/FsharpExperiments.Tests/NinetyNine.Tests.fs
@@ -6,10 +6,15 @@ open FsharpExperiments.Code.NinetyNine
 module `` NinetyNine problems solution tests`` = 
     [<Fact>]
     let `` Problem 1``() =
-        First.solve([1; 2; 3; 4]) |> should equal 4
-        First.solve(['x';'y';'z']) |> should equal 'z'
+        Problem1.solve([1; 2; 3; 4]) |> should equal 4
+        Problem1.solve(['x';'y';'z']) |> should equal 'z'
 
     [<Fact>]
     let ``Problem 2``() =
         Problem2.solve([1; 2; 3; 4]) |> should equal 3
         Problem2.solve(['x'..'z']) |> should equal 'y'
+
+    [<Fact>]
+    let `` Problem3``() =
+        Problem3.solve([1; 2; 3], 2) |> should equal 2
+        Problem3.solve("fsharp", 5) |>should equal "r" 
