1a62d85f5365a316c362ac954ef3cf1c1006516f
dominikus1993 <dominikus1910@gmail.com>
2016-07-31 21:06:48 +0200

    added solution for seventh prject euler problem

--- src/FsharpExperiments.Code/FsharpExperiments.Code.fs
+++ src/FsharpExperiments.Code/FsharpExperiments.Code.fs
@@ -4,6 +4,18 @@ module Euler1 =
     let solve (start, quantity) = 
         [start..quantity] |> List.filter(fun x -> x % 5 = 0 || x % 3 = 0) |> List.sum
 
+module Euler7 =
+    let private isPrime(num : int) =
+        match num with
+        |num when num < 2 -> false
+        |num when num = 2 -> true
+        |num -> [2..int(sqrt (double num))] |> Seq.filter(fun x -> num % x = 0) |> Seq.length = 0
+
+    let private generator = Seq.unfold(fun x -> Some(x, x + 1)) 0
+
+    let solve(num : int32) =
+        generator |> Seq.filter(isPrime) |> Seq.nth(num - 1)
+
 module Euler8 =
     let private getProduct(numbers : int64 seq) = 
         numbers |> Seq.windowed(13) |> Seq.map(fun nums -> nums |> Seq.fold(fun acc x -> acc * x) 1L) |> Seq.max
