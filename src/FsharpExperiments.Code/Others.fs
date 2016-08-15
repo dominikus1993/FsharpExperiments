e7d491204c565f0d8adbdf4b7c2bc2737938633d
dominikus1993 <dominikus1910@gmail.com>
2016-08-15 14:58:51 +0200

    added simple filter func

--- src/FsharpExperiments.Code/Others.fs
+++ src/FsharpExperiments.Code/Others.fs
@@ -3,3 +3,11 @@ namespace FsharpExperiments.Code.Others
 module PList =
     let pmap func list =
         list |> List.map(fun x -> async { return func x }) |> Async.Parallel |> Async.RunSynchronously |> Array.toList
+    
+    let filter func list =
+        let rec tailFilter l acc = 
+            match l with
+            |[] -> acc |> List.rev
+            |head :: tail -> 
+                if func(head) then tailFilter tail (head :: acc) else tailFilter tail acc
+        tailFilter list []
--- test/FsharpExperiments.Tests/Other.Tests.fs
+++ test/FsharpExperiments.Tests/Other.Tests.fs
@@ -8,3 +8,6 @@ module `` Parallel list functions test `` =
     [<Fact>]
     let ``Parallel map test``() =
         [1;2;3;4] |> PList.pmap (fun x -> x * 2) |> should equal [2;4;6;8]
+    [<Fact>]
+    let ``filter test``() =
+        [1;2;3;4] |> PList.filter(fun x -> x % 2 = 0) |> should equal [2;4]
